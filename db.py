7f42a1b7ab19813b17e105fc593323be128bfef4
Pratyush0511 <shitpratyush@gmail.com>
2025-07-25 21:16:14 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+
 
 client = MongoClient(
     MONGO_URI,
@@ -15,5 +15,6 @@ client = MongoClient(
     tlsCAFile=certifi.where()
 )
 
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 db = client[MONGO_DB_NAME]
 users_collection = db["users"]
