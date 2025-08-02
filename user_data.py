f50cf77fb5d3b847ab79ce7fdaf58963b9bc8be5
Pratyush0511 <shitpratyush@gmail.com>
2025-08-02 21:27:26 +0530

    Create user_data.py

---
+++ user_data.py
@@ -0,0 +1,5 @@
+# A simple in-memory store for user-specific data.
+# In a production environment, this would be a more persistent cache like Redis or a database.
+
+user_sessions = {}
+user_selected_hotels = {}
